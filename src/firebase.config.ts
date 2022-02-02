a4c58867850768e209764ae51d37066adffed8b0
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-02-02 13:14:27 +0530

    hello world

---
+++ src/firebase.config.ts
@@ -0,0 +1,10 @@
+import { FirebaseOptions } from "firebase/app";
+
+export const FirebaseConfig: FirebaseOptions = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import { initializeApp } from "firebase/app";
 
-import theme from '../theme'
-import { AppProps } from 'next/app'
+import theme from "../theme";
+import { AppProps } from "next/app";
+import { FirebaseConfig } from "../firebase.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  initializeApp(FirebaseConfig);
+
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
