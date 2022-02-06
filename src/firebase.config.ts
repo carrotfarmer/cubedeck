cf78a7cb2bfe672fa46eccd6c6daaa5c6848d5cb
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-02-06 15:49:28 +0530

    add object export to auth and db (netlify deploy err)

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -13,5 +13,7 @@ const firebaseConfig: FirebaseOptions = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
